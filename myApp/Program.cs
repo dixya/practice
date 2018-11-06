bc4213959022864d0bfd06e33cecb8a8e122692e
dixya <erdixya@gmail.com>
2018-11-05 18:38:43 -0800

    Following the tutorial of the documentation

---
+++ myApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace classes
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get; }
+        private static int accountNumberSeed = 1234567890;
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+        }
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Balance = initialBalance;
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+
+        static void Main(string[] args){
+            var account = new BankAccount("Dixya", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+            var account1 = new BankAccount("Dixya", 1000);
+            Console.WriteLine($"Account {account1.Number} was created for {account1.Owner} with {account1.Balance} initial balance.");
+        }
+
+    }
+}
